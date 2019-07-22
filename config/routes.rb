d7a6a4d773ee8643bc25341e51d500a4afbaecd9
quyctd <quy.dc98@gmail.com>
2019-07-23 00:13:08 +0700

    Signup user from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#home', page: 'home'
-  get 'about' => 'pages#about'
+  get 'about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: "users#new"
+  resources :users, expect: [:new]
+
 end
--- db/migrate/20190722012424_add_user_id_to_articles.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Migration for add user id to articles
-class AddUserIdToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_column :articles, :user_id, :integer
-  end
-end
