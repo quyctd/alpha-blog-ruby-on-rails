a8e9b1c0d402708fd478ba77b2b7142d79ffeea9
quyctd <quy.dc98@gmail.com>
2019-07-23 11:02:11 +0700

    Logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,26 @@
+# frozen_string_literal: true
+
+# Session controller
 class SessionsController < ApplicationController
 
-  def new
-
-  end
+  def new; end
 
   def create
-
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger] = 'Username or password not correct'
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to root_path
   end
 
 end
