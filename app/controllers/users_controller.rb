3faccdffcbd2e393a2ec382f07228894a5eaf574
quyctd <quy.dc98@gmail.com>
2019-07-23 22:13:51 +0700

    Add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: %i[edit update show]
-  before_action :require_user, except: %i[show index]
+  before_action :require_user, only: %i[edit update show]
   before_action :require_same_user, only: %i[edit update show]
 
   def index
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
