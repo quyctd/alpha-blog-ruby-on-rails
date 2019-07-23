dd72f21250fa74957f9c0d02aeaf4f0f2ac05368
quyctd <quy.dc98@gmail.com>
2019-07-23 09:35:05 +0700

    Listing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: %i[edit update show]
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
