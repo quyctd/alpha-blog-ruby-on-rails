084db62eb1cd8c4595bb73c4f351d232c5e320fc
quyctd <quy.dc98@gmail.com>
2019-07-23 10:38:22 +0700

    form login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  get 'signup', to: "users#new"
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, expect: [:new]
 
 end
