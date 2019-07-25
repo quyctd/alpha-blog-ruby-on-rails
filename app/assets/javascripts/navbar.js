83705a95b971e04eacb846169870291264ce0883
quyctd <quy.dc98@gmail.com>
2019-07-25 11:59:17 +0700

    Update css for card_name

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,12 @@
+console.log("custom js file loaded");
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 30) {
+        $('nav').addClass('navbar-light bg-light');
+        $('nav').removeClass('navbar-dark bg-transparent');
+    } else {
+        $('nav').removeClass('navbar-light bg-light');
+        $('nav').addClass('navbar-dark bg-transparent');
+    }
+});
+
