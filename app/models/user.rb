5cd6e689532f10a3a6031f99d719a1ce76b05675
quyctd <quy.dc98@gmail.com>
2019-07-26 16:08:27 +0700

    Update validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase }
-  validates :username, presence: true, length: { minimum: 3, maximum: 25 },
+  validates :username, presence: true, length: { minimum: 3, maximum: 50 },
                        uniqueness: { case_sensitive: false }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
-  validates :email, presence: true, length: { minimum: 3, maximum: 25 },
+  validates :email, presence: true, length: { minimum: 3, maximum: 50 },
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
