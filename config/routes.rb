bbe60d1a0ff03d131e8164dc3503cf686997d786
quyctd <quy.dc98@gmail.com>
2019-07-28 14:16:11 +0700

    Added feature image to category

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :users, expect: [:new]
 
-  resources :categories, except: [:destroy]
+  resources :categories, except: [:destroy] do
+    get 'serve', on: :member
+  end
 
 end
