8ccf1aa0fdd0d4d272554acb6c6c26098d8cf799
quyctd <quy.dc98@gmail.com>
2019-08-18 22:18:46 +0700

    Remove duplicate migrate

--- db/migrate/20190726094952_add_feature_image_to_category.rb
+++
@@ -1,5 +0,0 @@
-class AddFeatureImageToCategory < ActiveRecord::Migration[6.0]
-  def change
-    add_column :categories, :feature_image, :string, null: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_071817) do
+ActiveRecord::Schema.define(version: 2019_08_04_164523) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2019_07_28_071817) do
     t.binary "image_data"
     t.string "image_name"
     t.string "mime_type"
+  end
+
+  create_table "d_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_d_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_d_users_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
